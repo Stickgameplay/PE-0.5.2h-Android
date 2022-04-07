e5104d8fade08eb30a242499760544ec019c6103
Stickgameplay <96641303+Stickgameplay@users.noreply.github.com>
2022-04-07 18:44:51 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -67,16 +67,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['seu merda', 0.2], //From 0% to 19%
+		['+ ou -', 0.4], //From 20% to 39%
+		['ok', 0.5], //From 40% to 49%
+		['td bem', 0.6], //From 50% to 59%
+		['isso e bom', 0.69], //From 60% to 68%
+		['incrivel', 0.7], //69%
+		['vc e top!', 0.8], //From 70% to 79%
+		['uau legal!', 0.9], //From 80% to 89%
+		['Vc e uma lenda', 1], //From 90% to 99%
+		['impossivel!!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
 	public var modchartSprites:Map<String, ModchartSprite> = new Map<String, ModchartSprite>();
